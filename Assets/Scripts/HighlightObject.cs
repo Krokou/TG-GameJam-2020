bb89166e9c02d80b4bf3591ba3c6c4fbcf29ecf6
Nielspsj <nielspsj@hotmail.com>
2020-04-09 17:37:42 +0200

    HighlightObject
    Hover over object to highlight. Objects need the script and a coll/trigger.

---
+++ Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObject : MonoBehaviour
+{
+    //Changes the color of the material on the object the mouse hovers over if
+    // the object has this script on it and a collider. Collider can also be trigger.
+    private Color originalColor_C;
+    private Renderer curRenderer_R;
+
+    private void Start()
+    {
+        //Grab the current renderer on the object at start.
+        curRenderer_R = GetComponent<Renderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        //Store original color/texture to use when mouse stops hovering over the object.
+        originalColor_C = curRenderer_R.material.color;
+        //Change material color to what ever looks best.
+        curRenderer_R.material.color = Color.yellow;
+    }
+
+    private void OnMouseExit()
+    {
+        //Change color of matierial back to the original color/texture.
+        curRenderer_R.material.color = originalColor_C;
+    }
+}
