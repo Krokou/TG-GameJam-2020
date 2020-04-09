50a8eda21dace17bb9c05271753f652df75af65c
meelera <mariusror@hotmail.com>
2020-04-09 15:34:05 +0200

    Fixsed master volume memory between scenes

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -43,6 +43,11 @@ public class MenuController : MonoBehaviour
         Application.Quit();
     }
 
+    public void settingsBack()
+    {
+        settingPanel.gameObject.SetActive(false);
+    }
+
     
     // Start is called before the first frame update
     void Start()
