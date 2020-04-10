fbd17b1af5de321c2f05c39aad03d8ceb470af9d
Nielspsj <nielspsj@hotmail.com>
2020-04-10 09:37:32 +0200

    CollisionSound
    mute for the first second in mastergame scene.

--- Assets/Scripts/CollisionSound.cs
+++ Assets/Scripts/CollisionSound.cs
@@ -9,18 +9,31 @@ public class CollisionSound : MonoBehaviour
     public AudioSource Source;
     private float RandomPitch;
     private float RandomVolume;
+
+    private void Awake()
+    {
+        Source = GetComponent<AudioSource>();
+        Source.mute = true;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        Source = GetComponent<AudioSource>();
+        
+        StartCoroutine(StartSoundDelay());
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         RandomPitch = Random.Range(0.5f, 1.5f);
-        RandomVolume = Random.Range(0.2f, 1f);
+        RandomVolume = Random.Range(0.2f, 0.8f);
         Source.pitch = RandomPitch;
         Source.volume = RandomVolume;
         Source.Play();
+    }
+
+    private IEnumerator StartSoundDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        Source.mute = false;
     }
 }
---
+++ Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public GameObject player_Pref;
+    private GameObject player_GO;
+    public Transform spawnPos_TF;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player_GO = Instantiate(player_Pref, spawnPos_TF.position, spawnPos_TF.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
